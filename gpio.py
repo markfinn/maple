13a6b79d665408e8624add9b1f6a3f65cc376c6d
Mark Finn <mark@mfinn.net>
2021-03-09 13:33:18 +0000

    asyncio and signals

--- gpio.py
+++ gpio.py
@@ -1,45 +1,136 @@
 import gpiozero
-from time import sleep
 import logging
-
-log = logging.getLogger('maple')
-log.setLevel(logging.DEBUG)
-#ch = logging.StreamHandler()
-ch = logging.FileHandler('/home/mark/maple.log')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-log.addHandler(ch)
+import asyncio
+import signal, os, sys
 
 
+class Timer:
+    def __init__(self, timeout, callback):
+        self._timeout = timeout
+        self._callback = callback
+        self._task = asyncio.ensure_future(self._job())
 
-try:
+    async def _job(self):
+        await asyncio.sleep(self._timeout)
+        self._callback()
 
- airvac = gpiozero.DigitalOutputDevice(14, active_high=False)
- sapvac = gpiozero.DigitalOutputDevice(15, active_high=False)
- sapfloat = gpiozero.DigitalInputDevice(18, pull_up=True, active_state=None)
+    def cancel(self):
+        self._task.cancel()
+
+class AsyncDigitalInputDevice(gpiozero.DigitalInputDevice):
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self._awaiters = set()
+
+  async def wait_for_active(self, timeout=None, *, inverse=False):
+    #gpiozero is EDGE only, not level...  I want to detect either
+    if self.value == int(not inverse):
+      return
+    loop = asyncio.get_event_loop()
+    f = loop.create_future()
+    install = not self._awaiters
+    self._awaiters.add(f)
+    if install:
+      def active():
+        awaiters = self._awaiters
+        self._awaiters=set()
+        if inverse:
+          self.when_deactivated = None
+        else:
+          self.when_activated = None
+        for f in awaiters:
+          if not f.done():
+            f.set_result(None)
+
+      if inverse:
+        self.when_deactivated = lambda: loop.call_soon_threadsafe(active)
+      else:
+        self.when_activated = active
+        self.when_activated = lambda: loop.call_soon_threadsafe(active)
+
+      #gpiozero is EDGE only, not level...  I want to detect either
+      #and since the edge event is threaded we have to test again after we install to avoid a race
+      #note that active may get called twice, so we check done() on the future
+      if self.value == int(not inverse):
+        active()
+      
+    await asyncio.wait_for(f, timeout=timeout)
+
+  def wait_for_inactive(self, timeout=None):
+    return self.wait_for_active(inverse=True, timeout=timeout)
+
+async def main():
+
+  log = logging.getLogger('maple')
+  log.setLevel(logging.DEBUG)
+  ch = logging.StreamHandler()
+  #ch = logging.FileHandler('/home/mark/maple.log')
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  ch.setFormatter(formatter)
+  log.addHandler(ch)
 
 
- log.info('Starting')
- if sapfloat.value: 
-   log.info('draining')
-   sapvac.on()
-   sapfloat.wait_for_inactive(10)
- sapvac.off()
- airvac.on()
+  def handler(signum, frame):
+    log.warn('Signal handler called with signal', signum)
+    # Raises SystemExit(0):
+    sys.exit(0)
 
- log.info('Running')
- while True:
-  sapfloat.wait_for_active()
-  log.info('Sap Pump on')
-
-  sapvac.on()
-  sleep(20)
-  sapfloat.wait_for_inactive(120)
-  log.info('Sap Pump off')
-  sapvac.off()
+  signal.signal(signal.SIGINT, handler)
+  signal.signal(signal.SIGTERM, handler)
 
 
-except:
- log.exception('unhandled exception')
- raise
- 
+
+  try:
+
+    airvac = gpiozero.DigitalOutputDevice(14, active_high=False)
+    sapvac = gpiozero.DigitalOutputDevice(15, active_high=False)
+    sapfloat = AsyncDigitalInputDevice(18, pull_up=True, active_state=None)
+    romain = gpiozero.DigitalOutputDevice(23, active_high=False)
+    rossr = gpiozero.PWMOutputDevice(24, active_high=True, frequency=.3)
+    outvalve = gpiozero.DigitalOutputDevice(25, active_high=False)
+
+    romain.on()
+    rossr.value=1
+    await asyncio.sleep(900)
+    
+    sdfg
+    log.info('Starting')
+    if sapfloat.value:
+      log.info('draining')
+      sapvac.on()
+      await sapfloat.wait_for_inactive(120)
+    sapvac.off()
+    airvac.on()
+
+    log.info('Running')
+    while True:
+      await sapfloat.wait_for_active()
+      log.info('Sap Pump on')
+
+      sapvac.on()
+      while sapfloat.value:
+        await sapfloat.wait_for_inactive(120)
+        await asyncio.sleep(5)
+      
+      log.info('Sap Pump off')
+      sapvac.off()
+
+
+  except:
+    log.exception('unhandled exception')
+    raise
+
+  finally:
+    airvac.off()
+    sapvac.off()
+    romain.off()
+    rossr.off()
+    outvalve.off()
+    # watchdog.stop()
+
+
+if __name__ == '__main__':
+
+
+  asyncio.run(main())
+
