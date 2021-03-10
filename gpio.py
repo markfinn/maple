20ffb48f44de38fe1a3851551362d34d28ceb35b
Mark Finn <mark@mfinn.net>
2021-03-10 19:03:10 +0000

    quart, python3.9, http/2

--- gpio.py
+++ gpio.py
@@ -3,27 +3,24 @@ import logging
 import asyncio
 import signal, os, sys
 import watchdogdev
+import webapp
+import threading
+import contextlib
+import collections
+import time
 
-class Timer:
-    def __init__(self, timeout, callback):
-        self._timeout = timeout
-        self._callback = callback
-        self._task = asyncio.ensure_future(self._job())
 
-    async def _job(self):
-        await asyncio.sleep(self._timeout)
-        self._callback()
-
-    def cancel(self):
-        self._task.cancel()
-
+######################################################3
 class AsyncDigitalInputDevice(gpiozero.DigitalInputDevice):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self._awaiters = set()
 
+  def __str__(self):
+      return '%s'%self.value
+
   async def wait_for_active(self, timeout=None, *, inverse=False):
-    #gpiozero is EDGE only, not level...  I want to detect either
+    #gpiozero is EDGE only, not level...  I want to stop eaiting on either
     if self.value == int(not inverse):
       return
     loop = asyncio.get_event_loop()
@@ -48,7 +45,7 @@ class AsyncDigitalInputDevice(gpiozero.DigitalInputDevice):
         self.when_activated = active
         self.when_activated = lambda: loop.call_soon_threadsafe(active)
 
-      #gpiozero is EDGE only, not level...  I want to detect either
+      #gpiozero is EDGE only, not level...  I want to stop waiting on either
       #and since the edge event is threaded we have to test again after we install to avoid a race
       #note that active may get called twice, so we check done() on the future
       if self.value == int(not inverse):
@@ -59,24 +56,285 @@ class AsyncDigitalInputDevice(gpiozero.DigitalInputDevice):
   def wait_for_inactive(self, timeout=None):
     return self.wait_for_active(inverse=True, timeout=timeout)
 
+######################################################3
+class OverridableDigitalOutputDevice():
+  def __init__(self, *args, factory=gpiozero.DigitalOutputDevice, **kwargs):
+    self._dev = factory(*args, **kwargs)
+    self._overmode = 2#auto
+    self._value = self._dev.value
+    self._watchers = {}
+
+  def on(self):
+    self.value = 1
+
+  def off(self):
+    self.value = 0
+
+  @property
+  def value(self):
+    return self._value
+  @value.setter
+  def value(self, v):
+    self._value = v
+    if self._overmode == 2 and self._value != v:
+      self._dev.value = v
+      self._notify()
+
+  @property
+  def overmode(self):
+    return self._overmode
+  @overmode.setter
+  def overmode(self, v):
+    if v != self._overmode:
+      self._overmode = v
+    if self._overmode == 2:
+      self._dev.value = self._value
+    else:
+      self._dev.value = self._overmode
+    self._notify()
+
+
+  @contextlib.contextmanager
+  def watch(self, loop=None):
+    if loop is None:
+      loop = asyncio.get_event_loop()
+    queue = asyncio.Queue(loop=loop)
+    self._watchers[queue]=loop
+    try:
+      loop.call_soon_threadsafe(queue.put, self._overmode, self._value)
+      yield queue
+    finally:
+      del self._watchers[queue]
+
+  def _notify(self):
+    for loop, queue in self._watchers:
+      loop.call_soon_threadsafe(queue.put, (self._overmode, self._value))
+
+  def __str__(self):
+    if self._overmode == 2:
+      return '%s'%self._value
+    else:
+      return '%s <Overridden to %s>'%(self._value, self._overmode)
+
+
+
+log = logging.getLogger('maple')
+log.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+#ch = logging.FileHandler('/home/mark/maple.log')
+if os.getenv("INVOCATION_ID"):
+  #am under systemd for logging, journal has timestamp
+  formatter = logging.Formatter('%(levelname)s - %(message)s')
+else:
+  formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+
+
+class Maple():
+  def __init__(self):
+    self.airvac = OverridableDigitalOutputDevice(14, active_high=False)
+    self.sapvac = OverridableDigitalOutputDevice(15, active_high=False)
+    self.sapfloat = AsyncDigitalInputDevice(18, pull_up=True, active_state=None)
+    self.romain = OverridableDigitalOutputDevice(23, active_high=False)
+    self.rossr = OverridableDigitalOutputDevice(24, active_high=True, frequency=.3, factory = gpiozero.PWMOutputDevice)
+    self.outvalve = OverridableDigitalOutputDevice(25, active_high=False)
+
+  async def run(self):
+    try:
+      log.info('Setting Watchdog')
+      watchdog = watchdogdev.watchdog('/dev/watchdog')
+      watchdog.set_timeout(5)
+      if watchdog.get_boot_status():
+        log.error('Last boot was from Watchdog!!')  # not working?
+
+      async def wd_alive():
+        while True:
+          watchdog.keep_alive()
+          await asyncio.sleep(1)
+
+      wd_task = asyncio.ensure_future(wd_alive())
+
+      while 1:
+        await asyncio.sleep(5)
+      ASD
+
+      romain.on()
+      rossr.value = 1
+      await asyncio.sleep(900)
+
+      sdfg
+      if sapfloat.value:
+        log.info('draining')
+        sapvac.on()
+        await sapfloat.wait_for_inactive(120)
+      sapvac.off()
+      airvac.on()
+
+      log.info('Running')
+      while True:
+        await sapfloat.wait_for_active()
+        log.info('Sap Pump on')
+
+        sapvac.on()
+        while sapfloat.value:
+          await sapfloat.wait_for_inactive(120)
+          await asyncio.sleep(5)
+
+        log.info('Sap Pump off')
+        sapvac.off()
+
+
+    except SystemExit:
+      log.info('SystemExit')
+    except asyncio.CancelledError:
+      log.info('SystemExit')
+      raise
+    except:
+      log.exception('unhandled exception')
+      # raise
+
+    finally:
+      self.airvac.off()
+      self.sapvac.off()
+      self.romain.off()
+      self.rossr.off()
+      self.outvalve.off()
+
+      watchdog.keep_alive()
+      wd_task.cancel()
+      watchdog.magic_close()
+      log.info('Shutdown')
+
+
+
+
+
+#####
+async def runinotherthread():
+            await asyncio.wrap_future(asyncio.run_coroutine_threadsafe(prepop(list(self.oldlines)), loop))
+
+
+######################################################3
 async def main():
 
-  log = logging.getLogger('maple')
-  log.setLevel(logging.DEBUG)
 
-  ch = logging.StreamHandler()
-  #ch = logging.FileHandler('/home/mark/maple.log')
-  if os.getenv("INVOCATION_ID"):
-    #am under systemd for logging, journal has timestamp
-    formatter = logging.Formatter('%(levelname)s - %(message)s')
-  else:
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-  ch.setFormatter(formatter)
-  log.addHandler(ch)
 
+
+  log.info('Starting')
+
+  maple = Maple()
+
+  webloop = None
+  webshut = None
+  try:
+    def worker(mainloop):
+      new_loop = asyncio.new_event_loop()
+      asyncio.set_event_loop(new_loop)
+      shutdown_event = asyncio.Event()
+      nonlocal webloop
+      webloop = new_loop
+      nonlocal webshut
+      webshut = shutdown_event
+
+      # for sending to web
+      class FakeStream():
+        def __init__(self):
+          self.oldlines = collections.deque(maxlen=10)
+          self._watchers = {}
+          self.linecount = 0
+          self.idstart = str(time.time())+':'#so if we re-start, the lines dont srart at 0 and get lost
+        def write(self, d):
+          self.linecount += 1
+          evid = self.idstart+str(self.linecount)
+          self.oldlines.append((evid, d))
+          for loop, queue in self._watchers:
+            loop.call_soon_threadsafe(queue.put, (evid, d))
+
+        @contextlib.asynccontextmanager
+        async def watch(self):#can be called from wrong thread and loop
+          loop = asyncio.get_event_loop()
+          queue = asyncio.Queue()
+          initialqueue = asyncio.Queue()
+          todel = initialqueue
+          self._watchers[initialqueue]=loop
+          try:
+            for line in list(self.oldlines):
+              await queue.put(line)
+            while not initialqueue.empty():
+              line = await initialqueue.get()
+              await queue.put(line)
+            #tiny window here after while but before queue add where an event could get lost, but I don't care right now
+            self._watchers[queue]=loop
+            todel = queue
+            del self._watchers[initialqueue]
+            yield queue
+          finally:
+            del self._watchers[todel]
+
+      logqueue = FakeStream()
+      ch = logging.StreamHandler(stream=logqueue)
+      formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+      ch.setFormatter(formatter)
+      log.addHandler(ch)
+
+      log.info('running web server')
+      try:
+        webapp.app.config['mainloop'] = mainloop
+        webapp.app.config['maple'] = maple
+        webapp.app.config['logqueuer'] = logqueue
+        new_loop.run_until_complete(webapp.app.run_task(host="0.0.0.0", port=8443, certfile='fullchain.pem', keyfile='privkey.pem', debug=True, shutdown_trigger=shutdown_event.wait))
+      finally:
+        # being off in another therad is nice and all, but hypercorn sems to choke on asyncio.run() over here, and run until complete doesn't shut down in the end
+        # steal the shutdown code from asyncio.run
+        asyncio.runners._cancel_all_tasks(new_loop)
+        new_loop.run_until_complete(new_loop.shutdown_asyncgens())
+        new_loop.close()
+
+    threading.Thread(target=worker, args=(asyncio.get_event_loop(),)).start()
+
+  except:
+    log.exception('failed to start web service')
+
+  '''shutdown_event = asyncio.Event()
+
+def _signal_handler(*_: Any) -> None:
+        shutdown_event.set()
+
+loop = asyncio.get_event_loop()
+loop.add_signal_handler(signal.SIGTERM, _signal_handler)
+loop.run_until_complete(
+    serve(app, config, shutdown_trigger=shutdown_event.wait)
+)
+
+---or---
+
+import asyncio
+from signal import SIGINT, SIGTERM
+
+async def main_coro():
+    try:
+        await awaitable()
+    except asyncio.CancelledError:
+        do_cleanup()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    main_task = asyncio.ensure_future(main_coro())
+    for signal in [SIGINT, SIGTERM]:
+        loop.add_signal_handler(signal, main_task.cancel)
+    try:
+        loop.run_until_complete(main_task)
+    finally:
+        loop.close()
+'''
 
   def handler(signum, frame):
     log.warning('Signal handler called with signal %d', signum)
+    if webloop:
+      webloop.call_soon_threadsafe(webshut.set)
     # Raises SystemExit(0):
     sys.exit(0)
 
@@ -85,86 +343,11 @@ async def main():
 
 
 
-  try:
-    log.info('Starting')
+  await maple.run()
 
 
-    airvac = gpiozero.DigitalOutputDevice(14, active_high=False)
-    sapvac = gpiozero.DigitalOutputDevice(15, active_high=False)
-    sapfloat = AsyncDigitalInputDevice(18, pull_up=True, active_state=None)
-    romain = gpiozero.DigitalOutputDevice(23, active_high=False)
-    rossr = gpiozero.PWMOutputDevice(24, active_high=True, frequency=.3)
-    outvalve = gpiozero.DigitalOutputDevice(25, active_high=False)
-
-    log.info('Setting Watchdog')
-    watchdog=watchdogdev.watchdog('/dev/watchdog')
-    watchdog.set_timeout(5)
-    if watchdog.get_boot_status():
-      log.error('Last boot was from Watchdog!!')#not working?
-
-    async def wd_alive():
-      while True:
-        watchdog.keep_alive()
-        await asyncio.sleep(1)
-    wd_task = asyncio.ensure_future(wd_alive())
-
-
-
-#    while 1:
-#        await asyncio.sleep(5)
-    ASD
-
-
-    romain.on()
-    rossr.value=1
-    await asyncio.sleep(900)
-    
-    sdfg
-    if sapfloat.value:
-      log.info('draining')
-      sapvac.on()
-      await sapfloat.wait_for_inactive(120)
-    sapvac.off()
-    airvac.on()
-
-    log.info('Running')
-    while True:
-      await sapfloat.wait_for_active()
-      log.info('Sap Pump on')
-
-      sapvac.on()
-      while sapfloat.value:
-        await sapfloat.wait_for_inactive(120)
-        await asyncio.sleep(5)
-      
-      log.info('Sap Pump off')
-      sapvac.off()
-
-
-  except SystemExit:
-    log.info('SystemExit')
-  except asyncio.CancelledError:
-    log.info('SystemExit')
-    raise
-  except:
-    log.exception('unhandled exception')
-    #raise
-
-  finally:
-    airvac.off()
-    sapvac.off()
-    romain.off()
-    rossr.off()
-    outvalve.off()
-
-    watchdog.keep_alive()
-    wd_task.cancel()
-    watchdog.magic_close()
-    log.info('Shutdown')
-
-
+######################################################3
 if __name__ == '__main__':
-
 
   asyncio.run(main())
 
--- webapp.py
+++ webapp.py
@@ -1,31 +1,124 @@
+import asyncio
 import os
-from flask import Flask, redirect
-from subprocess import PIPE, Popen
+from quart import Quart, redirect, abort, make_response, request
+from quart_cors import cors
+import gpio
 
-app = Flask(__name__)
+app = Quart(__name__)
+app = cors(app, allow_origin='http://maple.bluesparc.net:3000')#  '*' works too
+
+
+class ServerSentEvent:
+
+    def __init__(
+            self,
+            data: str,
+            *,
+            event: str=None,
+            id: int=None,
+            retry: int=None,
+    ) -> None:
+        self.data = data
+        self.event = event
+        self.id = id
+        self.retry = retry
+
+    def encode(self) -> bytes:
+        message = f"data: {self.data}"
+        if self.event is not None:
+            message = f"{message}\nevent: {self.event}"
+        if self.id is not None:
+            message = f"{message}\nid: {self.id}"
+        if self.retry is not None:
+            message = f"{message}\nretry: {self.retry}"
+        message = f"{message}\r\n\r\n"
+        return message.encode('utf-8')
+
+    headers = {
+                'Content-Type'     : 'text/event-stream',
+                'Cache-Control'    : 'no-cache',
+                'Transfer-Encoding': 'chunked',
+            }
 
 @app.route("/")
-def index():
-    return redirect("/static/index.html", code=302)
+async def index():
+    return redirect("http://maple.bluesparc.net:3000", code=302)
  
 @app.route("/api/reboot", methods=['POST'])
-def reboot():
-    p = Popen("sudo reboot", shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p.communicate()
+async def reboot():
+    p = await asyncio.create_subprocess_shell("sudo reboot", shell=True, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await p.communicate()
     return stdout + stderr
 
 @app.route("/api/poweroff", methods=['POST'])
-def poweroff():
-    p = Popen("sudo poweroff", shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p.communicate()
+async def poweroff():
+    p = await asyncio.create_subprocess_shell("sudo poweroff", shell=True, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await p.communicate()
     return stdout + stderr
 
-@app.route("/maple.log")
-def log():
-    with open('/home/mark/maple.log', 'r') as f:
-     return f.read()
+@app.route("/api/log")
+async def log():
+    skipto = request.headers.get("Last-Event-ID")
+    async with app.config['logqueuer'].watch() as queue:
+        async def send_events():
+            while True:
+                evid, data = await queue.get()
+                if skipto is not None and evid <= skipto:
+                  continue
+                event = ServerSentEvent(data, id=evid)
+                yield event.encode()
 
- 
+        response = await make_response(send_events(), ServerSentEvent.headers)
+        response.timeout = None
+        return response
+
+@app.route("/api/outputs/<string:which>", methods=['GET', 'POST'])
+async def outputs(which):
+    try:
+        o = getattr(app.config['maple'], which)
+        assert isinstance(o, gpio.OverridableDigitalOutputDevice)
+    except:
+        abort(404)
+
+    if request.method == 'POST':
+#        if request.is_json
+#            val = request.json['value']
+        val = request.form['value']
+        o.overmode = val
+        return 'ok'
+
+    with o.watch(asyncio.get_event_loop()) as queue:
+        async def send_events():
+            while True:
+                data = await queue.get()
+                event = ServerSentEvent(data)
+                yield event.encode()
+
+        response = await make_response(send_events(), ServerSentEvent.headers)
+        response.timeout = None
+        return response
+
+@app.route("/api/outputs")
+async def outputsls():
+    response = {}
+    for n in dir(app.config['maple']):
+        o = getattr(app.config['maple'], n)
+        if isinstance(o, gpio.OverridableDigitalOutputDevice):
+            response[n] = str(o)
+
+    return response
+
+@app.route("/api/inputs")
+async def inputsls():
+    response = {}
+    for n in dir(app.config['maple']):
+        o = getattr(app.config['maple'], n)
+        if isinstance(o, gpio.AsyncDigitalInputDevice):
+            response[n] = str(o)
+
+    return response
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+#    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
     
